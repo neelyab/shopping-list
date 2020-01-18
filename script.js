b09f966e83219d27381b80a231e0057fb70c1722
Amanda <neelyab@vcu.edu>
2020-01-18 12:06:35 -0500

    append shopping items and added js file

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const shoppingItem= $(event.currentTarget).find('#shopping-list-entry').val();
+        $('.shopping-list').append($(`<li>
+        <span class="shopping-item"> ${shoppingItem} </span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+           <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+           <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`));
+
+    })
+    $('#')
+
+})
